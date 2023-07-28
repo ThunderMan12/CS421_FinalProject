c9564aad7d2a45d90938f3c232a407e9e3093463
ThunderMan12 <123035395+ThunderMan12@users.noreply.github.com>
2023-07-28 02:00:21 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask (__name__)
 
 @app.route ('/')
 def index():
-    return ""
+    return render_template('index.html')
+
+@app.route ('/login')
+def login():
+    return render_template('login.html')
+
+@app.route ('/itineraries')
+def itineraries():
+    return render_template('sampleIteneraries.html')
+
+@app.route ('/myprofile')
+def myprofile():
+    return render_template('userProfile.html')
+
+
 
 if __name__ == '__main__':
     app.run()
