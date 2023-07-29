38d199cb9d3de03b5b799115bd306b537893e83e
parmishekh <parmish@uab.edu>
2023-07-28 21:48:40 -0500

    overall html and main updates

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -11,7 +10,6 @@ db = SQLAlchemy(app)
 
 # Define the User model
 class User(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -31,91 +29,41 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
-# Check if the database file exists, and if not, create it along with tables
-if not os.path.exists('users.db'):
-    with app.app_context():
-        db.create_all()
-
-    def save(self):
-        with open(app.config['DATABASE_FILE'], 'a') as f:
-            f.write(f'{self.username}:{self.password}\n')
-
-    @staticmethod
-    def find_by_username(username):
-        with open(app.config['DATABASE_FILE'], 'r') as f:
-            for line in f:
-                data = line.strip().split(':')
-                if data[0] == username:
-                    return User(data[0], data[1])
-        return None
-
 @app.route('/login', methods=['POST'])
 def logins():
-    username = request.form['username']
-    password = request.form['password']
-
-    user = User.find_by_username(username)
-
-    if user:
-        if user.check_password(password):
-            return redirect(url_for('userprofile', username=username))
-        else:
-            return render_template('index.html', error='Invalid password')
-    else:
-        return render_template('index.html', error='Invalid username')
+    # ... (existing login code)
 
 @app.route('/register', methods=['POST'])
 def register():
     username = request.form['username']
+    email = request.form['email']
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
     password = request.form['password']
+    role = 'user'  # Set the default role for new users
 
-    # Check if username already exists
-    existing_user = User.find_by_username(username)
-    if existing_user:
-        return render_template('index.html', error='Username already exists')
+    # Check if username or email already exists
+    existing_user = User.query.filter_by(username=username).first()
+    existing_email = User.query.filter_by(email=email).first()
+    if existing_user or existing_email:
+        return render_template('index.html', error='Username or email already exists')
 
-    # Verify password requirements
+    # Verify password requirements (similar to the existing code)
     if not re.search(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$', password):
         return render_template('index.html', error='Invalid password. It must contain at least one lowercase letter, one uppercase letter, and end with a number.')
 
     try:
-        new_user = User(username=username, password=password)
-        new_user.save()
+        new_user = User(username=username, email=email, firstname=firstname, lastname=lastname, password=password, role=role)
+        db.session.add(new_user)
+        db.session.commit()
     except Exception as e:
         return render_template('index.html', error='An error occurred while creating the account.')
 
     return redirect(url_for('userprofile', username=username))
 
-@app.route('/users/<username>')
-def userprofile(username):
-    return render_template('user_profile.html', username=username)
-
-
-
-
-
-
-@app.route ('/')
-def index():
-    return render_template('index.html')
-
-@app.route ('/login')
-def login():
-    return render_template('login.html')
-
-@app.route ('/itianeraries')
-def itineraries():
-    return render_template('sampleIteneraries.html')
-
-@app.route ('/myprofile')
-def myprofile():
-    return render_template('userProfile.html')
-
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
-
-
+# ... (existing routes and view functions)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
+
